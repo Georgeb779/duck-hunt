43fccef46ba907e773c4f5ae6759d3ac91c701ba
George Baez <g.baez@gbh.com.do>
2022-11-07 21:04:11 -0400

    Create Duck component and integrate it

---
+++ src/components/Duck/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import DuckImage from "@/assets/duck_fly.png";
+
+export function Duck() {
+  return (
+    <div>
+      <img src={DuckImage} alt='duck' />
+    </div>
+  );
+}
